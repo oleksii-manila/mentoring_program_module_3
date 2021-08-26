9b34d110915df92765e8b6b9e9ef50630a013464
Oleksii Manila <Oleksii_Manila@epam.com>
2021-08-26 18:03:21 +0300

    WIP: need to adjust for big files

---
+++ Client/Program.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections;
+using System.IO;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            string filePath = @"C:\test";
+            watcher.Path = filePath;
+            watcher.EnableRaisingEvents = true;
+            watcher.NotifyFilter = NotifyFilters.FileName;
+            watcher.Filter = "*.*";
+
+            watcher.IncludeSubdirectories = true;
+
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+
+            new System.Threading.AutoResetEvent(false).WaitOne();
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            var path = e.FullPath;
+            var data = GetFileBits(path);
+
+            SendMessage(path, data);
+        }
+
+        private static byte[] GetFileBits(string path)
+        {
+            return File.ReadAllBytes(path);
+        }
+
+        private static void SendMessage(string path, byte[] data)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+            var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+            channel.ExchangeDeclare("test", ExchangeType.Fanout, true, true);
+            channel.BasicPublish("test", "", null, data);
+
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.IO;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+            var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("testQueue", true, false, false);
+            channel.QueueBind("testQueue", "test", "");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, args) =>
+            {
+                string Name = @"C:\testServer\test.pdf";
+
+                var Writer = new BinaryWriter(File.OpenWrite(Name));
+
+                Writer.Write(args.Body.ToArray());
+                Writer.Flush();
+                Writer.Close();
+            };
+
+            channel.BasicConsume("testQueue", true, consumer);
+
+            Console.ReadLine();
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
